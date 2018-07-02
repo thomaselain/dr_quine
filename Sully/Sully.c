331fd57b7f91d2ee3fef9fe6dfd5c47fdd845b79
Thomas <elainthomas0@gmail.com>
2018-07-02 13:29:22 +0200

    push

--- Sully/Sully.c
+++ Sully/Sully.c
@@ -2,5 +2,5 @@
 #include<string.h>
 #include<stdlib.h>
 int main(void){
-int n=10;
-if(n<=0){return(0);}char*s="#include<stdio.h>%c#include<string.h>%c#include<stdlib.h>%cint main(void){%cint n=%d;%cif(n<=0){return(0);}char*s=%c%s%c;char file[34];sprintf(file,%cSully_%cd.c%c,n-1);FILE *f=fopen(file,%cwb+%c);fprintf(f,s,10,10,10,10,n-1,10,34,s,34,34,37,34,34,34,34);return(0);}";char file[34],name[34],command[34];sprintf(name,"Sully_%d",n-1);sprintf(file,"%s.c",name);sprintf(command,"gcc -o %s %s",name,file);printf("\n\ncoucou voila la commande:%s\n\n",command);FILE *f=fopen(file,"wb+");fprintf(f,s,10,10,10,10,n-1,10,34,s,34,34,37,34,34,34,34);system(command);return(0);}
+int n=5;
+if(n<=0){return(0);}char*s="#include<stdio.h>%c#include<string.h>%c#include<stdlib.h>%cint main(void){%cint n=%d;%cif(n<=0){return(0);}char*s=%c%s%c;char file[34];sprintf(file,%cSully_%cd.c%c,n-1);FILE *f=fopen(file,%cwb+%c);fprintf(f,s,10,10,10,10,n-1,10,34,s,34,34,37,34,34,34,34);return(0);}";char file[34],name[34],command[34];sprintf(name,"Sully_%d",n-1);sprintf(file,"%s.c",name);sprintf(command,"gcc -o %s %s",name,file);printf("\n\ncoucou voila la commande:%s\n\n",command);FILE *f=fopen(file,"wb+");fprintf(f,s,10,10,10,10,n-1,10,34,s,34,34,37,34,34,34,34);system(command);return(0);} 
